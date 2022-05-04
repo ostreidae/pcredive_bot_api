c4ad475d6bdbdd8e8969e9aa68c72bb51dffece8
OppOops <ostreidae3310@gmail.com>
2022-05-04 23:53:38 +0800

    prefix format

--- cmds/maintain.py
+++ cmds/maintain.py
@@ -18,21 +18,21 @@ from discord.ext.commands.errors import CommandNotFound
 from core.errors import Errors
 help_message = \
 """
-【競技場功能】
-  [綁定Discord Id] --- {0}bind  {{自己的遊戲id}}
-  [查詢自己的 Id]  --- {0}me 
-  [查詢玩家Id]     --- {0}query {{遊戲id}}
+#【競技場功能】
+  [綁定Discord Id]  {0}bind  {{自己的遊戲id}}
+  [查詢自己的 Id]   {0}me 
+  [查詢玩家Id]      {0}query {{遊戲id}}
   
-【維護功能】
-  [從github更新版本] --- {0}update
-  [重新啟動]         --- {0}restart
-  [暫停查詢功能]     --- {0}stop
-  [下載檔案]         --- {0}download {{檔名路徑}} {{URL位址}}
-  [刪除檔案]         --- {0}delete {{檔名路徑}}
-  [查詢程序列表]     --- {0}list-child-process
-  [停止所有子程序]     --- {0}stop-child-process
-  [印出目前設定檔內容] --- {0}dump-config
-  [開發日誌]          --- {0}about
+#【維護功能】
+  [從github更新版本]  {0}update
+  [重新啟動]          {0}restart
+  [暫停查詢功能]      {0}stop
+  [下載檔案]          {0}download {{檔名路徑}} {{URL位址}}
+  [刪除檔案]          {0}delete {{檔名路徑}}
+  [查詢程序列表]      {0}list-child-process
+  [停止所有子程序]    {0}stop-child-process
+  [印出目前設定檔內容]  {0}dump-config
+  [開發日誌]           {0}about
 """
 with open('setting.json', 'r', encoding='utf8') as jfile:
 	jdata : dict = json.load(jfile)
@@ -44,7 +44,7 @@ prefix = jdata.get("Prefix", "")
 if type(prefix) is list:
     prefix = prefix[0] if len(prefix) >=1 else ""
  
-help_message = str.format(help_message, jdata.get("Prefix", ""))
+help_message = str.format(help_message, prefix)
 url = "https://raw.githubusercontent.com/ostreidae/pcredive_bot_api/main/version"
 download_url = "https://github.com/ostreidae/pcredive_bot_api/archive/refs/heads/main.zip"
 maintainer_id = jdata.get("Owner_id")
