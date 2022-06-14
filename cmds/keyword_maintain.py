6ada4717544645de4f63017586128660cce64f5f
OppOops <ostreidae3310@gmail.com>
2022-06-14 21:45:24 +0800

    remove unused messager

--- cmds/keyword_maintain.py
+++ cmds/keyword_maintain.py
@@ -116,8 +116,6 @@ class KeywordMaintain(Cog_Extension):
         ret = await self._on_message(message)
         if ret is None:
             await self.bot.process_commands(message)
-        else:
-            await message.channel.send("done")
     
     async def _on_message(self, message:discord.Message):
         content : str = message.content
